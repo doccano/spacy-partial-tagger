2d0215db8f70acb8878125abbd7281421155be34
yasufumi <yasufumi.taniguchi@gmail.com>
2022-04-18 16:45:29 +0900

    Remove TransformerTokenizer

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,35 +1,12 @@
 import pytest
 from spacy import Language
 
-from spacy_partial_tagger.tokenizer import CharacterTokenizer, TransformerTokenizer
-
-
-@pytest.fixture
-def transformer_tokenizer(nlp: Language) -> TransformerTokenizer:
-    return TransformerTokenizer(nlp.vocab, "distilroberta-base")
+from spacy_partial_tagger.tokenizer import CharacterTokenizer
 
 
 @pytest.fixture
 def character_tokenizer(nlp: Language) -> CharacterTokenizer:
     return CharacterTokenizer(nlp.vocab)
-
-
-def test_transformer_tokenizer(transformer_tokenizer: TransformerTokenizer) -> None:
-    doc = transformer_tokenizer("Tokyo is the capital of Japan.")
-    expected = [
-        "<s>",
-        "Tok",
-        "yo",
-        "Ġis",
-        "Ġthe",
-        "Ġcapital",
-        "Ġof",
-        "ĠJapan",
-        ".",
-        "</s>",
-    ]
-
-    assert [token.text for token in doc] == expected
 
 
 def test_character_tokenizer(character_tokenizer: CharacterTokenizer) -> None:
