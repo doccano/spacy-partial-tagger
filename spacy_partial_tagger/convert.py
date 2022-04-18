8642dd21d5d4e358d8cbffe31a3f50ed5e5d3950
yasufumi <yasufumi.taniguchi@gmail.com>
2022-04-18 14:55:49 +0900

    Update dataset converter

--- spacy_partial_tagger/convert.py
+++ spacy_partial_tagger/convert.py
@@ -1,53 +1,41 @@
 import json
 import os
 from argparse import ArgumentParser
-from collections import defaultdict
+from typing import List
 
 import spacy
 from spacy.tokens import Doc, DocBin
 from spacy.training.iob_utils import biluo_tags_to_spans
-from transformers import AutoTokenizer
 
 
-def main(input_path: str, output_path: str, model_name: str, lang: str) -> None:
+def converter(tokens: List[str], annotations: List[dict]) -> tuple:
+    char_tokens = list(" ".join(tokens))
+    token_index_to_char_index: list = [[] for _ in range(len(tokens))]
+    now = 0
+    for i, token in enumerate(tokens):
+        for char in token:
+            now = char_tokens.index(char, now)
+            token_index_to_char_index[i].append(now)
+    char_annotations = [
+        # [start, end)
+        (
+            token_index_to_char_index[annotation["start"]][0],
+            token_index_to_char_index[annotation["end"] - 1][-1] + 1,
+            annotation["type"],
+        )
+        for annotation in annotations
+    ]
+    return char_tokens, char_annotations
+
+
+def main(input_path: str, output_path: str, lang: str) -> None:
 
     nlp = spacy.blank(lang)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
     db = DocBin()
 
     with open(input_path) as f:
         for data in map(json.loads, f):
-            tokenized_data = tokenizer(  # type:ignore
-                " ".join(data["tokens"]),
-                add_special_tokens=True,
-                return_token_type_ids=False,
-                return_attention_mask=False,
-                return_offsets_mapping=True,
-            )
-            tokens = tokenized_data["input_ids"]
-            charidx2tokidx: dict = {}
-            for i, t in enumerate(data["tokens"]):
-                offset = len(charidx2tokidx) + i
-                for k in range(len(t)):
-                    charidx2tokidx[offset + k] = i
-
-            # Mapping derived from the offset mapping to go brom token idx to bpe idx
-            offset_mapping = tokenized_data.pop("offset_mapping")
-            tokidx2bpeidx = defaultdict(list)
-            for i, (s, e) in enumerate(offset_mapping):
-                if s == e:
-                    continue  # skip special tokens
-                tokidx = charidx2tokidx[s]
-                tokidx2bpeidx[tokidx].append(i)
-            annotations = [
-                # [start, end)
-                (
-                    tokidx2bpeidx[annotation["start"]][0],
-                    tokidx2bpeidx[annotation["end"] - 1][-1] + 1,
-                    annotation["type"],
-                )
-                for annotation in data["gold_annotations"]
-            ]
+            tokens, annotations = converter(data["tokens"], data["gold_annotations"])
             tags = ["O"] * len(tokens)
             for start, end, entity in annotations:
                 if any(tag != "O" for tag in tags[start:end]):
@@ -62,14 +50,10 @@ def main(input_path: str, output_path: str, model_name: str, lang: str) -> None:
                 )
             doc = Doc(
                 nlp.vocab,
-                words=tokenizer.convert_ids_to_tokens(tokens),
+                words=tokens,
                 spaces=[False] * len(tokens),
             )
-            ents = []
-            for span in biluo_tags_to_spans(doc, tags):
-                ent = doc.char_span(span.start_char, span.end_char, span.label_)
-                ents.append(ent)
-            doc.ents = ents  # type:ignore
+            doc.ents = biluo_tags_to_spans(doc, tags)  # type:ignore
             db.add(doc)
 
     db.to_disk(output_path)
@@ -78,9 +62,6 @@ def main(input_path: str, output_path: str, model_name: str, lang: str) -> None:
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--lang", type=str, default="en", help="Language")
-    parser.add_argument(
-        "--model", type=str, default="roberta-base", help="Transformer model"
-    )
     parser.add_argument("--input", type=str, required=True, help="An input file path")
     parser.add_argument("--output", type=str, default=None, help="An output file path")
 
@@ -92,4 +73,4 @@ if __name__ == "__main__":
     else:
         output = args.output
 
-    main(args.input, output, args.model, args.lang)
+    main(args.input, output, args.lang)
