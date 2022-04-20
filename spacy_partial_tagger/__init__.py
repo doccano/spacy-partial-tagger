fb93f0d4b1c859a87697e6935de185773e023d83
yasufumi <yasufumi.taniguchi@gmail.com>
2022-04-20 15:24:17 +0900

    Remove unnecessary imports

--- spacy_partial_tagger/__init__.py
+++
@@ -1,3 +0,0 @@
-from spacy_partial_tagger.architecture import build_partial_transformer_tagger  # NOQA
-from spacy_partial_tagger.pipeline import PartialEntityRecognizer  # NOQA
-from spacy_partial_tagger.tokenizer import CharacterTokenizer  # NOQA
