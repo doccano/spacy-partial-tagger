90f7f44877dda7d2715c8468f6f10c8acebe36de
yasufumi <yasufumi.taniguchi@gmail.com>
2022-05-12 09:36:38 +0900

    Apply black to pipeline.py

--- spacy_partial_tagger/pipeline.py
+++ spacy_partial_tagger/pipeline.py
@@ -194,7 +194,8 @@ class PartialEntityRecognizer(TrainablePipe):
 
         util.from_bytes(bytes_data, deserialize, exclude)
         self.model.initialize(
-            Y=({i: tag for i, tag in enumerate(self.id_to_tag)}, None))
+            Y=({i: tag for i, tag in enumerate(self.id_to_tag)}, None)
+        )
 
         model_deserializers = {
             "model": lambda b: self.model.from_bytes(b),
@@ -222,7 +223,8 @@ class PartialEntityRecognizer(TrainablePipe):
         )
         util.from_disk(path, deserialize, exclude)
         self.model.initialize(
-            Y=({i: tag for i, tag in enumerate(self.id_to_tag)}, None))
+            Y=({i: tag for i, tag in enumerate(self.id_to_tag)}, None)
+        )
         model_deserializers = {
             "model": load_model,
         }
