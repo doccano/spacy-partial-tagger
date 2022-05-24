7c6d323fa1671d167a81a76f06b1dee224d06523
yasufumi <yasufumi.taniguchi@gmail.com>
2022-05-24 10:05:52 +0900

    Add a test case for tokenize in convert_ja.py

--- tests/test_convert_ja.py
+++ tests/test_convert_ja.py
@@ -4,8 +4,16 @@ from spacy_partial_tagger.convert_ja import tokenize
 
 
 @pytest.mark.local
-def test_tokenize() -> None:
+def test_tokenize_returns_words_list() -> None:
     text = "外国人参政権に対する考え方の違い。"
     expected = ["外国", "人", "参政", "権", "に", "対する", "考え", "方", "の", "違い", "。"]
+
+    assert tokenize(text) == expected
+
+
+@pytest.mark.local
+def test_tokenize_skips_whitespace_if_included() -> None:
+    text = "ＴＨＥ\u3000ＦＩＲＳＴ\u3000ＴＡＫＥ"
+    expected = ["ＴＨＥ", "ＦＩＲＳＴ", "ＴＡＫＥ"]
 
     assert tokenize(text) == expected
