262aa477fe1f7fa4ed0b798c28ac0117533cc9c8
yasufumi <yasufumi.taniguchi@gmail.com>
2022-05-25 11:57:22 +0900

    Update convert_ja.py

--- tests/test_convert_ja.py
+++ tests/test_convert_ja.py
@@ -1,6 +1,6 @@
 import pytest
 
-from spacy_partial_tagger.convert_ja import tokenize
+from spacy_partial_tagger.convert_ja import tokenize_jumanpp
 
 
 @pytest.mark.local
@@ -8,7 +8,7 @@ def test_tokenize_returns_words_list() -> None:
     text = "外国人参政権に対する考え方の違い。"
     expected = ["外国", "人", "参政", "権", "に", "対する", "考え", "方", "の", "違い", "。"]
 
-    assert tokenize(text) == expected
+    assert tokenize_jumanpp(text) == expected
 
 
 @pytest.mark.local
@@ -16,4 +16,4 @@ def test_tokenize_skips_whitespace_if_included() -> None:
     text = "ＴＨＥ\u3000ＦＩＲＳＴ\u3000ＴＡＫＥ"
     expected = ["ＴＨＥ", "ＦＩＲＳＴ", "ＴＡＫＥ"]
 
-    assert tokenize(text) == expected
+    assert tokenize_jumanpp(text) == expected
