54a226106a108cab696b3f5aeb261d39dde6c54b
yasufumi <yasufumi.taniguchi@gmail.com>
2022-05-27 15:04:35 +0900

    Add to_jsonl to convert conull format to jsonl format

--- spacy_partial_tagger/util.py
+++ spacy_partial_tagger/util.py
@@ -1,6 +1,9 @@
+import json
 from typing import List
 
 import catalogue
+from conllu import parse_incr
+from spacy.training.iob_utils import tags_to_entities
 from spacy.util import registry
 from tokenizations import get_alignments as get_alignments_original
 
@@ -25,3 +28,39 @@ def get_alignments(source: List[str], target: List[str]) -> List[List[int]]:
                 if j in indices:
                     continue
     return y2x
+
+
+def to_jsonl(input_path: str, output_path: str) -> None:
+
+    outfile = open(output_path, "w")
+
+    with open(input_path) as infile:
+        for i, data in enumerate(parse_incr(infile)):
+            tags = ["O"] * len(data)
+            tokens = []
+            for i, x in enumerate(data):
+                if "NE" in x["misc"]:
+                    tags[i] = x["misc"]["NE"]
+                tokens.append(x["form"])
+            annotations = []
+            for label, start, end in tags_to_entities(tags):
+                annotations.append(
+                    {
+                        "start": start,
+                        "end": end + 1,
+                        "type": label,
+                        "kind": "entity",
+                        "mention": " ".join(tokens[start : end + 1]),
+                    }
+                )
+            new_data = {
+                "uid": f"{i}",
+                "tokens": tokens,
+                "gold_annotations": annotations,
+                "is_complete": True,
+            }
+
+            data_str = json.dumps(new_data)
+            outfile.write(f"{data_str}\n")
+
+    outfile.close()
