fae2f06d9d575a122c7581a5d82d3105b0e40dce
yasufumi <yasufumi.taniguchi@gmail.com>
2022-06-07 15:40:49 +0900

    Fix a test case for Decoder

--- tests/layers/test_decoder.py
+++ tests/layers/test_decoder.py
@@ -1,6 +1,6 @@
 import torch
-from partial_tagger.crf.decoders import ConstrainedViterbiDecoder
-from partial_tagger.crf.layer import CRF
+from partial_tagger.decoders.viterbi import ConstrainedViterbiDecoder
+from partial_tagger.encoders.linear import LinearCRFEncoder
 
 from spacy_partial_tagger.layers.decoder import ConstrainedDecoder, get_constraints
 
@@ -20,15 +20,14 @@ def test_constrained_decoder_returns_same_tensor_as_partial_tagger() -> None:
     start_constrains, end_constrains, transition_constraints = get_constraints(
         id_to_tag
     )
-    crf = CRF(128, len(id_to_tag))
+    crf = LinearCRFEncoder(128, len(id_to_tag))
     decoder1 = ConstrainedDecoder(
         start_constrains, end_constrains, transition_constraints
     )
     decoder2 = ConstrainedViterbiDecoder(
-        crf, start_constrains, end_constrains, transition_constraints
+        start_constrains, end_constrains, transition_constraints
     )
-    text_features = torch.randn(3, 11, 128)
+    embeddings = torch.randn(3, 11, 128)
+    log_potentials = crf(embeddings)
 
-    _, expected = decoder2(text_features)
-
-    assert torch.allclose(decoder1(crf(text_features)), expected)
+    assert torch.allclose(decoder1(log_potentials), decoder2(log_potentials))
