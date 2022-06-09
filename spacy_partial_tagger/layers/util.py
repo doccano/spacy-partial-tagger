5962bb6dcdd2730ee76bdc7ded2de555feac46a7
yasufumi <yasufumi.taniguchi@gmail.com>
2022-06-09 13:48:53 +0900

    Add docstring to util.py

--- spacy_partial_tagger/layers/util.py
+++ spacy_partial_tagger/layers/util.py
@@ -4,6 +4,7 @@ import torch
 def get_mask(
     lengths: torch.Tensor, max_length: int, device: torch.device
 ) -> torch.Tensor:
+    """Converts length tensor to mask tensor."""
     return (
         torch.arange(
             max_length,
