36fa9cc2ad618c4db08b3fb29d794fe280ef4230
yasufumi <yasufumi.taniguchi@gmail.com>
2023-01-10 16:04:53 +0900

    Update util.py

--- spacy_partial_tagger/util.py
+++ spacy_partial_tagger/util.py
@@ -5,28 +5,44 @@ import spacy_alignments as tokenizations
 from spacy.tokens import Doc, Span
 from spacy.training.iob_utils import biluo_tags_to_offsets
 from spacy.util import registry
+from transformers import PreTrainedTokenizer
 
 registry.label_indexers = catalogue.create(  # type:ignore
     "spacy", "label_indexers", entry_points=True
 )
 
 
-def get_alignments(source: List[str], target: List[str]) -> List[List[int]]:
-    _, y2x = tokenizations.get_alignments(source, target)
-    indices = iter(range(len(source)))
-    for y in y2x:
-        if not y:
-            # TODO: Fix me, maybe this doesn't work properly when [UNK] corresponds
-            # more than 1 letter.
-            for i in indices:
-                if source[i] != " ":
-                    y.append(i)
-                    break
+def get_alignments(
+    tokenizer: PreTrainedTokenizer, text: str, input_ids: List[int]
+) -> list:
+    tokens = tokenizer.word_tokenizer.tokenize(
+        text, never_split=tokenizer.all_special_tokens
+    )
+    _, y2x = tokenizations.get_alignments(text, tokens)
+    token2char = {i: (x[0], x[-1] + 1) for i, x in enumerate(y2x)}
+
+    pieces = [
+        (i, tokenizer.convert_ids_to_tokens(piece))
+        for i, piece in enumerate(input_ids)
+        if piece not in {tokenizer.cls_token_id, tokenizer.sep_token_id}
+    ]
+
+    mapping: List[List[Tuple[int, int]]] = [[] for _ in range(len(input_ids))]
+    now = 0
+    for i, token in enumerate(tokens):
+        for subword in tokenizer.subword_tokenizer.tokenize(token):
+            if pieces[now][1] == subword:
+                mapping[pieces[now][0]].append(token2char[i])
+                now += 1
+
+    res: List[List[int]] = []
+    for m in mapping:
+        if not m:
+            res.append([])
         else:
-            for j in y:
-                if j in indices:
-                    continue
-    return y2x
+            res.append(list(range(m[0][0], m[-1][1])))
+
+    return res
 
 
 def make_char_based_doc(doc: Doc, tags: List[str]) -> Doc:
